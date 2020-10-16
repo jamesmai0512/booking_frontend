0fb5890247cd50924c7b9015ec33c903a71fb0aa
jamesmai0512 <jamesmai.sg@gmail.com>
2020-10-16 20:14:04 +0800

    Create datepicker

--- src/pages/NewMeeting.js
+++ src/pages/NewMeeting.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import StartDate from "../components/StartDate";
+import EndDate from "../components/EndDate";
 import axiox from "axios";
 import {
   Col,
@@ -14,11 +16,20 @@ import { useHistory } from "react-router-dom";
 const NewMeetings = () => {
   const [nameRequire, setNameRequire] = useState(false);
   const [titleRequire, setTitleRequire] = useState(false);
+  const [timeRequire, setTimeRequire] = useState(false);
+
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
 
   const [newMeeting, setNewMeeting] = useState({
     user_name: "",
     title: "",
+    time_meeting: "",
+    start_date: "",
+    end_date: "",
   });
+
+  // const [date, setDate] = useState(new Date());
 
   let history = useHistory();
 
@@ -68,17 +79,62 @@ const NewMeetings = () => {
         </Col>
       </FormGroup>
 
+      <FormGroup row>
+        <Label sm="2">Meeting Times</Label>
+        <Col>
+          <Input
+            type="text"
+            placeholder="00:00:00"
+            onChange={(event) => {
+              setNewMeeting({
+                ...newMeeting,
+                time_meeting: event.target.value,
+              });
+            }}
+            invalid={timeRequire}
+          ></Input>
+          {timeRequire && <FormFeedback>Time is require</FormFeedback>}
+        </Col>
+      </FormGroup>
+
+      <StartDate
+        FormGroup={FormGroup}
+        startDate={startDate}
+        setStartDate={setStartDate}
+        setNewMeeting={setNewMeeting}
+        newMeeting={newMeeting}
+        endDate={endDate}
+      />
+
+      <EndDate
+        FormGroup={FormGroup}
+        endDate={endDate}
+        setEndDate={setEndDate}
+        setNewMeeting={setNewMeeting}
+        newMeeting={newMeeting}
+        startDate={startDate}
+      />
+
       <Button
         type="button"
         color="success"
         onClick={() => {
-          const { user_name, title } = newMeeting;
+          const {
+            user_name,
+            title,
+            time_meeting,
+            start_date,
+            end_date,
+          } = newMeeting;
 
           if (user_name !== "" && title !== "") {
             axiox
               .post("http://localhost:3001/meetings", {
                 user_name: user_name,
                 title: title,
+                time_meeting: time_meeting,
+                start_date: start_date,
+                end_date: end_date,
               })
               .then((response) => {
                 console.log(response);
@@ -92,6 +148,9 @@ const NewMeetings = () => {
             }
             if (title === "") {
               setTitleRequire(true);
+            }
+            if (time_meeting === "") {
+              setTimeRequire(true);
             }
           }
         }}
